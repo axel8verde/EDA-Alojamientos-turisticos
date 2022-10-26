e3343d1cd799ec65ba46d81ba2c69bbb388ea9da
axel8verde <112900700+axel8verde@users.noreply.github.com>
2022-10-26 23:59:02 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -7,7 +7,7 @@ from streamlit_folium import st_folium
 import plotly.express as px
 import seaborn as sns
 from folium import plugins 
-import zipfile
+
 
 
 
@@ -27,6 +27,7 @@ def intro():
 
 def carga_datos():
     # Leer CSV
+    
     with zipfile.ZipFile('listings_completo.zip') as zip_ref:
         zip_ref.extract('listings_completo.csv')
     global data 
